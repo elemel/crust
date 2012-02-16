c9bbafb822a31252b94872522824fd3ee0d1e11d
Mikael Lind <elemel@elemel.se>
2012-02-16 16:04:54 +0100

    Make p1 and p2 of IntBox2 public

--- src/int_geometry.hpp
+++ src/int_geometry.hpp
@@ -1,37 +1,42 @@
 #ifndef CRUST_INT_GEOMETRY_HPP
 #define CRUST_INT_GEOMETRY_HPP
 
+#include "int_math.hpp"
+
 #include <algorithm>
 #include <limits>
 
 namespace crust {
     class IntBox2 {
     public:
+        IntVector2 p1;
+        IntVector2 p2;
+
         IntBox2() :
-            minX_(std::numeric_limits<int>::max()),
-            minY_(std::numeric_limits<int>::max()),
-            maxX_(std::numeric_limits<int>::min()),
-            maxY_(std::numeric_limits<int>::min())
+            p1(std::numeric_limits<int>::max(),
+               std::numeric_limits<int>::max()),
+            p2(std::numeric_limits<int>::min(),
+               std::numeric_limits<int>::min())
         { }
         
         int getX() const
         {
-            return minX_;
+            return p1.x;
         }
         
         int getY() const
         {
-            return minY_;
+            return p1.y;
         }
         
         int getWidth() const
         {
-            return (minX_ < maxX_) ? maxX_ - minX_ : 0;
+            return (p1.x < p2.x) ? p2.x - p1.x : 0;
         }
         
         int getHeight() const
         {
-            return (minY_ < maxY_) ? maxY_ - minY_ : 0;
+            return (p1.y < p2.y) ? p2.y - p1.y : 0;
         }
         
         int getSize() const
@@ -41,35 +46,29 @@ namespace crust {
         
         bool isEmpty() const
         {
-            return maxX_ <= minX_ || maxY_ <= minY_;
+            return p2.x <= p1.x || p2.y <= p1.y;
         }
         
         bool contains(int x, int y) const
         {
-            return minX_ <= x && x < maxX_ && minY_ <= y && y < maxY_;
+            return p1.x <= x && x < p2.x && p1.y <= y && y < p2.y;
         }
         
         void pad(int dx, int dy)
         {
-            minX_ -= dx;
-            minY_ -= dy;
-            maxX_ += dx;
-            maxY_ += dy;
+            p1.x -= dx;
+            p1.y -= dy;
+            p2.x += dx;
+            p2.y += dy;
         }
         
         void add(int x, int y)
         {
-            minX_ = std::min(x, minX_);
-            minY_ = std::min(y, minY_);
-            maxX_ = std::max(x + 1, maxX_);
-            maxY_ = std::max(y + 1, maxY_);
+            p1.x = std::min(x, p1.x);
+            p1.y = std::min(y, p1.y);
+            p2.x = std::max(x + 1, p2.x);
+            p2.y = std::max(y + 1, p2.y);
         }
-        
-    private:
-        int minX_;
-        int minY_;
-        int maxX_;
-        int maxY_;
     };
 }
 
