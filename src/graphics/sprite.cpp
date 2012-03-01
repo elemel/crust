75a4f0574e076b6c214a89ea8225bda5b3587030
Mikael Lind <elemel@elemel.se>
2012-03-01 20:51:11 +0100

    Faster and better looking AO calc, also draw half a pixel inside

--- src/graphics/sprite.cpp
+++ src/graphics/sprite.cpp
@@ -95,39 +95,66 @@ namespace crust {
             glDeleteTextures(1, &normalAndShadowTexture_);
         }
         glGenTextures(1, &normalAndShadowTexture_);
-        
-        std::vector<GLbyte> normalAndShadowData;
+
+        std::vector<float> shadowData;
         for (int dy = -4; dy < 2 * height + 4; ++dy) {
             for (int dx = -4; dx < 2 * width + 4; ++dx) {
-
-                // TODO: Normals.
-                normalAndShadowData.push_back(0);
-                normalAndShadowData.push_back(0);
-                normalAndShadowData.push_back(127);
-                
                 float shadow = 0.0f;
                 Vector2 position = Vector2(0.5f * float(dx), 0.5f * float(dy));
-                for (int ddy = 0; ddy < 6; ++ddy) {
-                    for (int ddx = 0; ddx < 6; ++ddx) {
-                        if (ddy * ddy + ddx * ddx < 6 * 6) {
-                            Vector2 samplePosition = position + Vector2(0.5f * float(ddx) - 1.25f, 0.5f * float(ddy) - 1.25f);
-                            float alpha = float(pixels_.getElement(x + int(samplePosition.x + 1.5f) - 1,
-                                                                   y + int(samplePosition.y + 1.5f) - 1).alpha) / 255.0f;
-                            float minSquaredDistance = std::min(std::min(getSquaredDistance(position,
-                                                                                            samplePosition + Vector2(-0.25, -0.25f)),
-                                                                         getSquaredDistance(position,
-                                                                                            samplePosition + Vector2(0.25, -0.25f))),
-                                                                std::min(getSquaredDistance(position,
-                                                                                            samplePosition + Vector2(0.25, 0.25f)),
-                                                                         getSquaredDistance(position,
-                                                                                            samplePosition + Vector2(-0.25, 0.25f))));
-                            shadow = std::max(shadow, alpha / std::max(1.0f, 10.0f * minSquaredDistance));
-                        }
+                for (int ddy = 0; ddy < 2; ++ddy) {
+                    for (int ddx = 0; ddx < 2; ++ddx) {
+                        Vector2 samplePosition = position + Vector2(0.5f * float(ddx) - 0.25f, 0.5f * float(ddy) - 0.25f);
+                        float alpha = float(pixels_.getElement(x + int(samplePosition.x + 1.5f) - 1,
+                                                               y + int(samplePosition.y + 1.5f) - 1).alpha) / 255.0f;
+                        shadow = std::max(shadow, alpha);
                     }
                 }
-                shadow = 0.7f * shadow;
-                normalAndShadowData.push_back(std::min(127, int(shadow * 128.0)));
+                shadowData.push_back(0.8f * shadow);
             }
+        }
+
+        std::vector<float> smoothShadowData(shadowData.size(), 0.0f);
+        int pitch = 2 * width + 8;
+        for (int dy = 2; dy < 2 * height + 8 - 2; ++dy) {
+            for (int dx = 2; dx < 2 * width + 8 - 2; ++dx) {
+                float shadow = shadowData[dy * pitch + dx];
+                if (0.001f < shadow) {
+                    smoothShadowData[dy * pitch + dx] = std::max(shadow, smoothShadowData[dy * pitch + dx]);
+
+                    smoothShadowData[dy * pitch + dx + 1] = std::max(0.5f * shadow, smoothShadowData[dy * pitch + dx + 1]);
+                    smoothShadowData[dy * pitch + dx - 1] = std::max(0.5f * shadow, smoothShadowData[dy * pitch + dx - 1]);
+                    smoothShadowData[(dy + 1) * pitch + dx] = std::max(0.5f * shadow, smoothShadowData[(dy + 1) * pitch + dx]);
+                    smoothShadowData[(dy - 1) * pitch + dx] = std::max(0.5f * shadow, smoothShadowData[(dy - 1) * pitch + dx]);
+
+                    smoothShadowData[(dy - 1) * pitch + dx - 1] = std::max(0.3f * shadow, smoothShadowData[(dy - 1) * pitch + dx - 1]);
+                    smoothShadowData[(dy - 1) * pitch + dx + 1] = std::max(0.3f * shadow, smoothShadowData[(dy - 1) * pitch + dx + 1]);
+                    smoothShadowData[(dy + 1) * pitch + dx + 1] = std::max(0.3f * shadow, smoothShadowData[(dy + 1) * pitch + dx + 1]);
+                    smoothShadowData[(dy + 1) * pitch + dx - 1] = std::max(0.3f * shadow, smoothShadowData[(dy + 1) * pitch + dx - 1]);
+
+                    smoothShadowData[dy * pitch + dx + 2] = std::max(0.2f * shadow, smoothShadowData[dy * pitch + dx + 2]);
+                    smoothShadowData[dy * pitch + dx - 2] = std::max(0.2f * shadow, smoothShadowData[dy * pitch + dx - 2]);
+                    smoothShadowData[(dy + 2) * pitch + dx] = std::max(0.2f * shadow, smoothShadowData[(dy + 2) * pitch + dx]);
+                    smoothShadowData[(dy - 2) * pitch + dx] = std::max(0.2f * shadow, smoothShadowData[(dy - 2) * pitch + dx]);
+
+                    smoothShadowData[(dy - 1) * pitch + dx - 2] = std::max(0.1f * shadow, smoothShadowData[(dy - 1) * pitch + dx - 2]);
+                    smoothShadowData[(dy - 1) * pitch + dx + 2] = std::max(0.1f * shadow, smoothShadowData[(dy - 1) * pitch + dx + 2]);
+                    smoothShadowData[(dy + 1) * pitch + dx + 2] = std::max(0.1f * shadow, smoothShadowData[(dy + 1) * pitch + dx + 2]);
+                    smoothShadowData[(dy + 1) * pitch + dx - 2] = std::max(0.1f * shadow, smoothShadowData[(dy + 1) * pitch + dx - 2]);
+
+                    smoothShadowData[(dy - 2) * pitch + dx - 1] = std::max(0.1f * shadow, smoothShadowData[(dy - 2) * pitch + dx - 1]);
+                    smoothShadowData[(dy - 2) * pitch + dx + 1] = std::max(0.1f * shadow, smoothShadowData[(dy - 2) * pitch + dx + 1]);
+                    smoothShadowData[(dy + 2) * pitch + dx + 1] = std::max(0.1f * shadow, smoothShadowData[(dy + 2) * pitch + dx + 1]);
+                    smoothShadowData[(dy + 2) * pitch + dx - 1] = std::max(0.1f * shadow, smoothShadowData[(dy + 2) * pitch + dx - 1]);
+                }
+            }
+        }
+
+        std::vector<GLbyte> normalAndShadowData(4 * smoothShadowData.size());
+        for (std::size_t i = 0; i < smoothShadowData.size(); ++i) {
+            normalAndShadowData[4 * i + 0] = 0;
+            normalAndShadowData[4 * i + 1] = 0;
+            normalAndShadowData[4 * i + 2] = 127;
+            normalAndShadowData[4 * i + 3] = std::min(127, int(smoothShadowData[i] * 128.0));
         }
 
         glBindTexture(GL_TEXTURE_2D, normalAndShadowTexture_);
@@ -155,12 +182,16 @@ namespace crust {
         float y1 = float(pixels_.getY() - 2);
         float x2 = float(pixels_.getX() + pixels_.getWidth() + 2);
         float y2 = float(pixels_.getY() + pixels_.getHeight() + 2);
-
+        float width = x2 - x1;
+        float height = y2 - y1;
+        float halfTexWidth = 0.5f / width;
+        float halfTexHeight = 0.5f / height;
+        
         Vector2 vertices[] = {
-            transformPoint(transform, Vector2(x1, y1)),
-            transformPoint(transform, Vector2(x2, y1)),
-            transformPoint(transform, Vector2(x2, y2)),
-            transformPoint(transform, Vector2(x1, y2))
+            transformPoint(transform, Vector2(x1 + 0.5f, y1 + 0.5f)),
+            transformPoint(transform, Vector2(x2 - 0.5f, y1 + 0.5f)),
+            transformPoint(transform, Vector2(x2 - 0.5f, y2 - 0.5f)),
+            transformPoint(transform, Vector2(x1 + 0.5f, y2 - 0.5f))
         };
 
         for (int i = 0; i < 4; ++i) {
@@ -168,15 +199,15 @@ namespace crust {
             vertexArray_[i * 2 + 1] = vertices[i].y;
         }
 
-        texCoordArray_[0] = 0.0f;
-        texCoordArray_[1] = 0.0f;
-        texCoordArray_[2] = 1.0f;
-        texCoordArray_[3] = 0.0f;
-        texCoordArray_[4] = 1.0f;
-        texCoordArray_[5] = 1.0f;
-        texCoordArray_[6] = 0.0f;
-        texCoordArray_[7] = 1.0f;
-
+        texCoordArray_[0] = 0.0f + halfTexWidth;
+        texCoordArray_[1] = 0.0f + halfTexHeight;
+        texCoordArray_[2] = 1.0f - halfTexWidth;
+        texCoordArray_[3] = 0.0f + halfTexHeight;
+        texCoordArray_[4] = 1.0f - halfTexWidth;
+        texCoordArray_[5] = 1.0f - halfTexHeight;
+        texCoordArray_[6] = 0.0f + halfTexWidth;
+        texCoordArray_[7] = 1.0f - halfTexHeight;
+        
         for (int i = 0; i < 4; ++i) {
             colorArray_[i * 4 + 0] = color_.red;
             colorArray_[i * 4 + 1] = color_.green;
